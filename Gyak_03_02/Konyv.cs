0d179d3dc646e58bb99de1ab6d60423b9760a061
Bandita69 <aaandris96@gmail.com>
2022-03-02 21:37:15 +0100

    hhhhhhhhhhhhhhhhhhhhheeeeeeeeeeeeeeeeyyyyyyyyyyyyy

---
+++ Gyak_03_02/Konyv.cs
@@ -0,0 +1,97 @@
+﻿/* 2. Készítsen Könyv osztályt és egy Könyv osztályt használó osztályt.
+a) A Könyv osztály adattagjai: cím, szerző, megjelenés éve, ára. Legyen egy metódusa, amely a
+paraméterként kapott százalékos értékkel növeli a könyv árát. Legyen egy metódusa, ami egy
+sztringbe összefűzi az adatokat és ezt adja vissza. Írjunk setter metódusokat az adatok beállításához,
+és getter metódusokat az adatok lekérdezéséhez (adatrejtés → ellenőrzött adathozzáférés).
+b) A Könyv osztályt használó osztályban hozzunk létre egy könyvet és kérdezzük le az adatait */
+
+using System;
+using System.Text.RegularExpressions;
+namespace konyvek
+{
+    public class Konyv
+    {
+        private String cim;
+        private String szerzo;
+        private int megjelenes;
+        private int ar;
+
+        public String getCim()
+        {
+            return this.cim;
+        }
+
+        public void setCim(String cim)
+        {
+            this.cim = cim;
+        }
+
+        public String getSzerzo()
+        {
+            return this.szerzo;
+        }
+
+        public void setSzerzo(String szerzo)
+        {
+            this.szerzo = szerzo;
+        }
+
+        public int getMegjelenes()
+        {
+            return this.megjelenes;
+        }
+
+        public void setMegjelenes(int megjelenes)
+        {
+            this.megjelenes = megjelenes;
+        }
+
+        public int getAr()
+        {
+            return this.ar;
+        }
+
+        public void setAr(int ar)
+        {
+            this.ar = ar;
+        }
+
+
+        public Konyv(string cim, string szerzo, int megjelenes, int ar)
+        {
+            this.cim = cim;
+            this.szerzo = szerzo;
+            this.megjelenes = megjelenes;
+            this.ar = ar;
+        }
+        public override String ToString() { return "Könyv :" + cim + " " + szerzo + " " + megjelenes + " " + ar; }
+
+        public void szazalekosAremelkedes(int szazalek)
+        {
+            setAr(ar + ar* szazalek / 100);
+
+
+        }
+
+
+
+
+
+    }
+
+    public class Konyv_osztaly_hasznalo_osztaly
+    {
+
+
+        public static void Main(string[] args)
+        {
+            Konyv k = new Konyv("Nudlikevero Harmaik Bovitett kiadas", "Gordonka", 1800, 1000);
+            System.Console.WriteLine(k);
+            k.szazalekosAremelkedes(10);
+            System.Console.WriteLine(k);
+
+        }
+    }
+
+
+}
---
+++ Gyak_03_03/Program.cs
@@ -0,0 +1,109 @@
+﻿/* Házi feladat:
+a) Készítsen egy Személy osztályt az alábbi adatokkal: név, súly (egész, kilogrammban megadva),
+magasság (valós, méterben megadva). Legyen egy metódusa, amely visszaadja a testtömeg indexet
+(tti=tomeg/(m*m)). Legyen egy metódusa, amely visszaad egy alkatot jellemző szöveget:
+"sovány": tti<18,5
+"kövér": tti>25
+"normál": egyébként
+Legyen egy metódusa, amely egy sztringbe összefűzi az adatokat és visszaadja azokat az alábbi
+alakban: név, súly, magasság, tti: alkat.
+Készítsen egy futtatható osztályt, amelyben beolvassa egy személy adatait (név, súly, magasság) és
+kiírja azokat összefűzve a kiszámított testtömeg indexszel és alkattal.
+b) Írjunk setter metódusokat az adatok beállításához, és getter metódusokat az adatok
+lekérdezéséhez (adatrejtés → ellenőrzött adathozzáférés).
+ */
+
+using System;
+using System.Text.RegularExpressions;
+namespace szemelyek
+{
+    public class Szemely
+    {
+
+        String name = "";
+        int suly;
+        double magassag;
+        double tti;
+
+
+        String alkat = "normál";
+
+        public Szemely(String name, int suly, double magassag)
+        {
+            this.name = name;
+            this.suly = suly;
+            this.magassag = magassag;
+            this.tti = (suly / (magassag * magassag));
+        }
+
+
+        public String getAlkat()
+        {
+            return this.alkat;
+        }
+
+
+        public double getTti()
+        {
+            return this.tti;
+        }
+
+
+
+        public String getName()
+        {
+            return this.name;
+        }
+
+        public void setName(String name)
+        {
+            this.name = name;
+        }
+
+        public int getSuly()
+        {
+            return this.suly;
+        }
+
+        public void setSuly(int suly)
+        {
+            this.suly = suly;
+        }
+
+        public double getMagassag()
+        {
+            return this.magassag;
+        }
+
+        public void setMagassag(double magassag)
+        {
+            this.magassag = magassag;
+        }
+
+        public string testTomegIndex()
+        {
+
+            if (tti > 25) { alkat = "kövér"; }
+            if (tti < 18.5) { alkat = "sovány"; }
+
+
+            return alkat;
+        }
+
+        public override String ToString() { return "Program :" + name + " " + suly + " " + magassag + " " + tti + " " + testTomegIndex(); }
+
+
+
+    }
+    public class szemelyMain
+    {
+        public static void Main(string[] args)
+        {
+            Szemely sz = new Szemely("Bandika", 85, 1.9);
+            System.Console.WriteLine(sz);
+            System.Console.WriteLine("tti külön " + sz.getTti());
+
+        }
+
+    }
+}
