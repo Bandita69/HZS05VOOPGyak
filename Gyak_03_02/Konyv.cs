614063be06f6b19e25a7b91a3597433a1494ff4e
Bandita69 <aaandris96@gmail.com>
2022-03-19 20:51:46 +0100

    KÖNYV HÁZI SQL

--- Gyak_03_02/Konyv.cs
+++ Gyak_03_02/Konyv.cs
@@ -13,8 +13,21 @@ namespace konyvek
     {
         private String cim;
         private String szerzo;
-        private int megjelenes;
+        static readonly int megjelenes = DateTime.Today.Year;
         private int ar;
+        private int page;
+
+        public int getPage()
+        {
+            return this.page;
+        }
+
+        public void setPage(int page)
+        {
+            this.page = page;
+        }
+
+        static string kiado = "Móra";
 
 
 
@@ -45,16 +58,6 @@ namespace konyvek
             this.szerzo = szerzo;
         }
 
-        public int getMegjelenes()
-        {
-            return this.megjelenes;
-        }
-
-        public void setMegjelenes(int megjelenes)
-        {
-            this.megjelenes = megjelenes;
-        }
-
         public int getAr()
         {
             return this.ar;
@@ -66,15 +69,16 @@ namespace konyvek
         }
 
 
-        public Konyv(string cim, string szerzo, int megjelenes, int ar)
+        public Konyv(string cim, string szerzo, int megjelenes, int ar, int page)
         {
             this.cim = cim;
             this.szerzo = szerzo;
-            this.megjelenes = megjelenes;
+            this.page = page;
+
             this.ar = ar;
         }
 
-        public Konyv(string cim, string szerzo) : this(cim, szerzo, DateTime.Now.Year, 2500)
+        public Konyv(string cim, string szerzo, int page) : this(cim, szerzo, DateTime.Now.Year, 2500, page)
         {
 
 
@@ -84,7 +88,7 @@ namespace konyvek
 
 
 
-        public override String ToString() { return "Könyv :" + cim + " " + szerzo + " " + megjelenes + " " + ar; }
+        public override String ToString() { return "Könyv :" + cim + " " + szerzo + " " + megjelenes + " " + ar + " " + kiado + " " + page; }
 
         public void szazalekosAremelkedes(int szazalek)
         {
@@ -93,6 +97,27 @@ namespace konyvek
 
         }
 
+        public static Konyv hosszabb(Konyv egyik, Konyv masik)
+        {
+
+            if (egyik.getPage() > masik.getPage())
+            {
+                return egyik;
+            }
+            else { return masik; }
+
+        }
+
+        public bool paros()
+        {
+
+
+            if (getPage() % 2 == 0)
+            {
+                return true;
+            }
+            else { return false; }
+        }
 
 
 
@@ -105,10 +130,81 @@ namespace konyvek
 
         public static void Main(string[] args)
         {
-            Konyv k = new Konyv("Nudlikevero Harmaik Bovitett kiadas", "Gordonka");
+            Konyv k = new Konyv("Nudlikevero Harmaik Bovitett kiadas", "Gordonka", 100);
             System.Console.WriteLine(k);
             k.szazalekosAremelkedes(10);
             System.Console.WriteLine(k);
+
+            System.Console.WriteLine("Másik feladatok: \n");
+
+            var tomb = new Konyv[3];
+
+
+
+
+
+
+            tomb[0] = new Konyv("Nudlikevero Harmaik Bovitett kiadas", "Gordonka", 100);
+            tomb[1] = new Konyv("Magyar Cinke fajok gondozása", "Gálik Tamás", 151);
+            tomb[2] = new Konyv("Hogyan hagyjuk abba az izgulást és szeressük az oroszokat", "Gordonka", 120);
+            Konyv legho = tomb[0];
+            for (var i = 0; i < 3; i++)
+            {
+                legho = Konyv.hosszabb(legho, tomb[i]);
+
+            }
+
+            System.Console.WriteLine("leghosszabb " + legho);
+
+            Konyv parosLegho = tomb[0];
+            for (var i = 0; i < 3; i++)
+            {
+
+                if (tomb[i].paros())
+                {
+                    parosLegho = Konyv.hosszabb(parosLegho, tomb[i]);
+                }
+
+
+            }
+
+            System.Console.WriteLine("paros leghosszabb " + parosLegho);
+
+
+
+
+            var playersPerTeam =
+                from Konyv in tomb
+                group Konyv by Konyv.getSzerzo() into playerGroup
+                select new
+                {
+                    Name = playerGroup.Key,
+                    Count = playerGroup.Count(),
+                };
+
+                foreach (var item in playersPerTeam)
+                {
+                    System.Console.WriteLine(item);
+                }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
