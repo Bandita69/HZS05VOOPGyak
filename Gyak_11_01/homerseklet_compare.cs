c8ce445a90e68323c2b9dc19bedd175c606a7586
Bandita69 <aaandris96@gmail.com>
2022-05-04 16:59:31 +0200

    gyakorlas

---
+++ Gyak_11_01/homerseklet_compare.cs
@@ -0,0 +1,199 @@
+﻿using System;
+namespace homero
+{
+    public enum mertekegyseg
+    {
+        CELSIUS,
+        KELVIN
+    }
+    public class Homerseklet : IComparable<Homerseklet>
+    {
+
+
+
+        double homerseklet;
+
+        mertekegyseg _mert;
+
+        public double getHomerseklet()
+        {
+            return this.homerseklet;
+        }
+
+        public void setHomerseklet(double homerseklet)
+        {
+            this.homerseklet = homerseklet;
+        }
+
+        public mertekegyseg get_mert()
+        {
+            return this._mert;
+        }
+
+        public void set_mert(mertekegyseg _mert)
+        {
+            this._mert = _mert;
+        }
+
+
+
+        public Homerseklet(double homerseklet, mertekegyseg mert)
+        {
+            this.homerseklet = homerseklet;
+            this._mert = mert;
+        }
+        public Homerseklet(double homerseklet)
+        {
+            this.homerseklet = homerseklet;
+            _mert = mertekegyseg.CELSIUS;
+        }
+
+        public static double homersekletetKonvertal(mertekegyseg _mert, double homerseklet)
+        {
+
+            return _mert == mertekegyseg.CELSIUS ? homerseklet + 273.15 : homerseklet - 273.15;
+        }
+
+        public int CompareTo(Homerseklet hom)
+        {
+
+            double temp = 0;
+            double temp_this = 0;
+            if (hom.get_mert() == mertekegyseg.CELSIUS)
+            {
+                temp = Homerseklet.homersekletetKonvertal(hom.get_mert(), hom.getHomerseklet());
+
+            }
+            else
+            {
+                temp = hom.getHomerseklet();
+            }
+
+            if (get_mert() == mertekegyseg.CELSIUS)
+            {
+                temp_this = Homerseklet.homersekletetKonvertal(get_mert(), getHomerseklet());
+
+            }
+            else
+            {
+                temp_this = getHomerseklet();
+            }
+
+            return (int)(temp_this - temp);
+
+
+
+
+        }
+
+
+        public override String ToString() { return "Homerseklet : " + _mert + " " + homerseklet; }
+
+
+
+    }
+    public class HomersekletProba
+    {
+        public static void Main(string[] args)
+        {
+            var tomb = new Homerseklet[4];
+            var my = new HomersekletProba();
+
+            Random rnd = new Random();
+
+            tomb[0] = new Homerseklet(my.readDouble(), my.readString());
+            tomb[1] = new Homerseklet(my.readDouble(), my.readString());
+            tomb[2] = new Homerseklet(rnd.NextDouble() * 100, mertekegyseg.CELSIUS);
+            tomb[3] = new Homerseklet(rnd.NextDouble() * 100, mertekegyseg.CELSIUS);
+            my.tombKiir(tomb);
+
+
+            System.Console.WriteLine(Homerseklet.homersekletetKonvertal(tomb[0].get_mert(), tomb[0].getHomerseklet()));
+            System.Console.WriteLine(Homerseklet.homersekletetKonvertal(tomb[1].get_mert(), tomb[1].getHomerseklet()));
+            System.Console.WriteLine(my.atlagHomersekletKelvin(tomb));
+
+
+
+            foreach (var item in tomb)
+            {
+                System.Console.WriteLine(item);
+            }
+
+            Array.Sort(tomb);
+
+            foreach (var item in tomb)
+            {
+                System.Console.WriteLine(item);
+            }
+
+            System.Console.WriteLine(Array.BinarySearch(tomb,new Homerseklet(15)));
+
+            List<Homerseklet> holista = new List<Homerseklet>(tomb);
+
+            holista.Sort();
+            System.Console.WriteLine(holista.BinarySearch(new Homerseklet(15)));
+
+
+        }
+
+
+
+        double atlagHomersekletKelvin(Homerseklet[] tomb)
+        {
+            double atlag = 0;
+            double temp = 0;
+
+            foreach (var item in tomb)
+            {
+                if (item.get_mert() == mertekegyseg.CELSIUS)
+                {
+                    temp = Homerseklet.homersekletetKonvertal(item.get_mert(), item.getHomerseklet());
+
+                }
+                else
+                {
+                    temp = item.getHomerseklet();
+                }
+                atlag += temp;
+            }
+
+            return atlag / tomb.Length;
+        }
+        void tombKiir(Homerseklet[] tomb)
+        {
+
+            foreach (var item in tomb)
+            {
+                System.Console.WriteLine(item);
+            }
+
+        }
+        double readDouble()
+        {
+            double a;
+            System.Console.WriteLine("Adjon meg egy szamot\n");
+            while (!(double.TryParse(Console.ReadLine(), out a)))
+            {
+                System.Console.WriteLine("Tobbe-itt-ne-lassam");
+            }
+
+            return a;
+        }
+
+        mertekegyseg readString()
+        {
+            mertekegyseg a;
+            System.Console.WriteLine("Adjon meg egy mertekegyseget\n");
+            while (!(Enum.TryParse<mertekegyseg>(Console.ReadLine(), out a)))
+            {
+                System.Console.WriteLine("Tobbe-itt-ne-lassam");
+            }
+
+
+            return a;
+        }
+
+    }
+
+
+}
---
+++ Gyak_11_02/Program.cs
@@ -0,0 +1,202 @@
+﻿using System;
+namespace myCard
+{
+
+
+    public class Card
+    {
+        private string Owner;
+        private DateTime Validation;
+
+        public enum Bank
+        {
+            OTP,
+            KANDH,
+            ERSTE,
+            CIB
+        }
+
+        private Bank _bank;
+
+        public Bank get__bank()
+        {
+            return this._bank;
+        }
+
+        public void set_bank(Bank _bank)
+        {
+            this._bank = _bank;
+        }
+
+        private int Balance;
+
+        public Card(string owner, DateTime validation, Bank bank, int balance)
+        {
+            Owner = owner;
+            Validation = validation;
+            _bank = bank;
+            Balance = balance;
+
+        }
+
+        public string getOwner()
+        {
+            return this.Owner;
+        }
+
+        public void setOwner(string Owner)
+        {
+            this.Owner = Owner;
+        }
+
+        public DateTime getValidation()
+        {
+            return this.Validation;
+        }
+
+        public void setValidation(DateTime Validation)
+        {
+            this.Validation = Validation;
+        }
+
+
+        public int getBalance()
+        {
+            return this.Balance;
+        }
+
+        public void setBalance(int balance)
+        {
+            this.Balance = balance;
+        }
+
+        public virtual bool withDrawal(int amount)
+        {
+
+            if (amount > Balance || Validation < DateTime.Now)
+            {
+                return false;
+            }
+            else
+            {
+                Balance -= amount;
+                return true;
+            }
+
+
+        }
+
+
+
+        public override String ToString() { return "Card :" + Owner + " " + Validation + " " + Balance + " " + _bank; }
+
+
+
+
+    }
+    public class CreditCard : Card, IComparable<CreditCard>
+    {
+        private int CreditLine;
+
+
+        public int getCreditLine()
+        {
+            return this.CreditLine;
+        }
+
+        public void setCreditLine(int CreditLine)
+        {
+            this.CreditLine = CreditLine;
+        }
+
+        public CreditCard(string owner, DateTime validation, Bank bank, int balance) : base(owner, validation, bank, balance)
+        {
+            CreditLine = 100000;
+        }
+        public CreditCard(string owner, DateTime validation, Bank bank, int balance, int creditLine) : base(owner, validation, bank, balance)
+        {
+            CreditLine = creditLine;
+        }
+
+        public override bool withDrawal(int amount)
+        {
+
+            if (!(base.withDrawal(amount)) && amount < CreditLine)
+            {
+
+                CreditLine -= (amount - base.getBalance());
+                base.setBalance(0);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
+        }
+
+        public int CompareTo(CreditCard cc)
+        {
+
+
+            return this.getValidation().CompareTo(cc.getValidation());
+
+        }
+
+        public override String ToString() { return base.ToString() + " " + CreditLine; }
+
+    }
+
+
+}
+
+namespace ujNevter
+{
+    public class CardTest
+    {
+        public static void Main(string[] args)
+        {
+
+            var my2 = new myCard.CreditCard[4];
+            my2[0] = new myCard.CreditCard("kiss tamas", new DateTime(2020, 04, 30), myCard.Card.Bank.OTP, 150000, 100000);
+            my2[1] = new myCard.CreditCard("Nagy Levente", new DateTime(2022, 05, 31), myCard.Card.Bank.ERSTE, 100000);
+            my2[2] = new myCard.CreditCard("Szabó László,", new DateTime(2019, 03, 31), myCard.Card.Bank.OTP, 200000, 100000);
+            my2[3] = new myCard.CreditCard("Kovács edit", new DateTime(2021, 01, 31), myCard.Card.Bank.CIB, 250000);
+
+            System.Console.WriteLine(my2[0].withDrawal(280000));
+            System.Console.WriteLine(my2[0]);
+            System.Console.WriteLine(my2[1].withDrawal(80000));
+            System.Console.WriteLine(my2[1]);
+            System.Console.WriteLine(my2[2].withDrawal(50000));
+            System.Console.WriteLine(my2[2]);
+            System.Console.WriteLine(my2[3].withDrawal(100000));
+            System.Console.WriteLine(my2[3]);
+
+
+            void countCib(myCard.CreditCard[] tomb)
+            {
+                int sum = 0;
+                foreach (var item in tomb)
+                {
+                    if (item.get__bank() == myCard.Card.Bank.CIB)
+                    {
+                        sum++;
+                    }
+                }
+                System.Console.WriteLine(sum);
+
+            }
+
+            countCib(my2);
+
+            Array.Sort(my2);
+
+            foreach (var item in my2)
+            {
+                System.Console.WriteLine(item.getValidation());
+            }
+
+        }
+    }
+}
